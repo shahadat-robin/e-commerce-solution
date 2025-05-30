e649f3de00a49d068793ab72691115a80c034958
Robin <connection.robin@gmail.com>
2025-05-31 02:11:52 +0600

    newsletter done

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,12 +1,14 @@
 import type { PropsWithChildren } from "react";
 import Footer from "./footer";
 import Header from "./header";
+import NewsLetter from "./newsletter";
 
 export default function AppLayout({ children }: PropsWithChildren) {
   return (
     <>
       <Header />
       <main>{children}</main>
+      <NewsLetter />
       <Footer />
     </>
   );
