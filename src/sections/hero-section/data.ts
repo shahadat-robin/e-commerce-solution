b14c73db33421c6ce2c7402b9b17688c2935b0d2
Robin <connection.robin@gmail.com>
2025-06-01 12:20:35 +0600

    hero section done

--- src/sections/hero-section/data.ts
+++ src/sections/hero-section/data.ts
@@ -1,1 +1,13 @@
-export const heroMedia = [];
+export const mediaBanners = [
+  "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-001-267x240.png",
+  "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-002-267x240.png",
+  "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-001-267x240.png",
+  "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-002-267x240.png",
+];
+
+export const carouselBanners = [
+  "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-001-267x240.png",
+  "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-002-267x240.png",
+  "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-001-267x240.png",
+  "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-002-267x240.png",
+];
--- src/sections/hero-section/index.tsx
+++ src/sections/hero-section/index.tsx
@@ -1,33 +1,19 @@
 import { Carousel } from "antd";
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
+import { carouselBanners, mediaBanners } from "./data";
 
 export default function HeroSection() {
   return (
-    <section>
-      <div className="container grid grid-cols-4 gap-5 py-5">
-        <div>
-          <Image
-            src={
-              "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-001-267x240.png"
-            }
-            alt=""
-            height={300}
-            width={300}
-            className="w-full h-auto"
-          />
-        </div>
-        <div className="row-start-2 col-start-1">
-          <Image
-            src={
-              "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-002-267x240.png"
-            }
-            alt=""
-            height={300}
-            width={300}
-            className="w-full h-auto"
-          />
-        </div>
-        <div className="col-span-2 row-span-2">
+    <section className="py-5">
+      <div className="container grid grid-cols-4 gap-5">
+        {mediaBanners.map((banner, index) => (
+          <div key={index} className={twMerge("", index === 1 && "row-start-2 col-start-1")}>
+            <Image src={banner} alt="" height={300} width={300} className="w-full h-auto" />
+          </div>
+        ))}
+
+        <div className="col-span-2 row-span-2 row-start-1 col-start-2">
           <Carousel
             className="h-full"
             rootClassName="h-full"
@@ -37,65 +23,17 @@ export default function HeroSection() {
             adaptiveHeight
             draggable
           >
-            <Image
-              src={
-                "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-001-267x240.png"
-              }
-              alt=""
-              height={500}
-              width={500}
-              className="w-full h-auto"
-            />
-            <Image
-              src={
-                "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-002-267x240.png"
-              }
-              alt=""
-              height={500}
-              width={500}
-              className="w-full h-auto"
-            />
-            <Image
-              src={
-                "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-001-267x240.png"
-              }
-              alt=""
-              height={500}
-              width={500}
-              className="w-full h-auto"
-            />
-            <Image
-              src={
-                "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-002-267x240.png"
-              }
-              alt=""
-              height={500}
-              width={500}
-              className="w-full h-auto"
-            />
+            {carouselBanners.map((banner, index) => (
+              <Image
+                key={index}
+                src={banner}
+                alt=""
+                height={500}
+                width={500}
+                className="w-full h-auto"
+              />
+            ))}
           </Carousel>
-        </div>
-        <div>
-          <Image
-            src={
-              "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-002-267x240.png"
-            }
-            alt=""
-            height={300}
-            width={300}
-            className="w-full h-auto"
-          />
-        </div>
-        <div>
-          <Image
-            src={
-              "https://inspiretemplates.com/E-shope/image/cache/catalog/right-banner/267X240-001-267x240.png"
-            }
-            alt=""
-            height={300}
-            width={300}
-            className="w-full h-auto"
-          />
         </div>
       </div>
     </section>
