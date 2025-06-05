0226d46aa6904074a1fafbb9c3e3e682b2e034aa
Robin <connection.robin@gmail.com>
2025-06-06 01:14:41 +0600

    redux toolkit installed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
+import { store } from "@/store";
 import "@/styles/globals.css";
 import theme from "@/utils/theme";
 import { ConfigProvider } from "antd";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 import AppLayout from "../layout";
 
 export default function App(props: AppProps) {
@@ -9,9 +11,11 @@ export default function App(props: AppProps) {
 
   return (
     <ConfigProvider theme={theme}>
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
+      <Provider store={store}>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </Provider>
     </ConfigProvider>
   );
 }
