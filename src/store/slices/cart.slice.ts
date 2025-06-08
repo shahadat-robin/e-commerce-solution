fdd0c2e0ce716c9dc50693fee74346ed2d8ab914
Robin <connection.robin@gmail.com>
2025-06-08 23:50:34 +0600

    cart slice initialized

---
+++ src/store/slices/cart.slice.ts
@@ -0,0 +1,52 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+  totalQuantity: 0,
+  totalPrice: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      const item = action.payload;
+      //   const existing = state.items.find((i) => i.id === item.id);
+
+      //   if (existing) {
+      //     existing.quantity += 1;
+      //   } else {
+      //     state.items.push({ ...item, quantity: 1 });
+      //   }
+
+      state.totalQuantity += 1;
+      state.totalPrice += item.price;
+    },
+
+    removeItem(state, action) {
+      const id = action.payload;
+      //   const existing = state.items.find((i) => i.id === id);
+
+      //   if (existing) {
+      //     state.totalQuantity -= 1;
+      //     state.totalPrice -= existing.price;
+
+      //     if (existing.quantity === 1) {
+      //       state.items = state.items.filter((i) => i.id !== id);
+      //     } else {
+      //       existing.quantity -= 1;
+      //     }
+      //   }
+    },
+
+    clearCart(state) {
+      state.items = [];
+      state.totalQuantity = 0;
+      state.totalPrice = 0;
+    },
+  },
+});
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
